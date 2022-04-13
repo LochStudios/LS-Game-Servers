a78adea7ee0296e261cc8ec019975f7154e01628
Lachlan Murdoch <lachlan@lochstudios.com>
2022-04-13 11:48:22 +1000

    Update DayZServer.bat

--- DayZServer.bat
+++ DayZServer.bat
@@ -1,8 +1,7 @@
 @echo off
-:start
-cls
+:handles
 ::Server name (This is just for the bat file)
-set serverName=[AU] Sentral.Network Dayz Test Server
+set serverName=[AU] Sentral.Network Dayz Server Starter
 ::Server files location
 set serverLocation="C:\Program Files (x86)\Steam\steamapps\common\DayZServer"
 ::Server Port
@@ -13,14 +12,21 @@ set serverConfig=serverDZ.cfg
 set serverCPU=4
 ::Sets title for terminal (DONT edit)
 title %serverName% batch
-::DayZServer location (DONT edit)
-cd "%serverLocation%"
-echo (%time%) %serverName% started.
+cls
+
+:start
+echo (%time:~0,-6%) %serverName% started.
 ::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
 start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -port=%serverPort% "-profiles=config" "-mod=@CF;@Community-Online-Tools;@[AU] Sentral.Network Dayz LoadingScreen;@Airdrop-Upgraded;@InventoryPlusPlus;@BulletStacksPlusPlus;@Advanced Weapon Scopes;@TeddysWeaponPack;@VanillaPlusPlusMap;@TraderFixesAndFeatures;@BaseBuildingPlus;@Code Lock;@Unlimited Stamina;@DinkyItems;@Trader" -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
-::Time in seconds before kill server process (14400 = 4 hours)
-timeout 21600
-cls
+
+:TimedRestart
+IF %time:~0,-6%==06:00 goto kill
+IF %time:~0,-6%==12:00 goto kill
+IF %time:~0,-6%==18:00 goto kill
+IF %time:~0,-6%==00:00 goto kill
+goto TimedRestart
+
+:kill
 taskkill /im DayZServer_x64.exe /F
 ::Time in seconds to wait before..
 timeout 10
